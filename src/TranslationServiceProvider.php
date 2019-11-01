a0ae7ad66e8cbfad3e48f5cabd5a84cf65f622f2
omar hendawi <omar.fathy13@gmail.com>
2019-11-01 02:44:12 +0200

    dev

---
+++ src/TranslationServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Mngez\Generator;
+
+use Illuminate\Translation\TranslationServiceProvider as BaseTranslationServiceProvider;
+
+use Mngez\Generator\Translator;
+
+class TranslationServiceProvider extends BaseTranslationServiceProvider {
+
+
+ 
+  
+
+}
